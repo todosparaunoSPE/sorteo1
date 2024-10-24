edc08301661203ecdca95b8d15bd0bc0629e9a2f
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-10-24 11:55:19 -0600

    Update app1.py

--- app1.py
+++ app1.py
@@ -39,13 +39,7 @@ except Exception as e:
 page_bg_img = """
 <style>
 [data-testid="stAppViewContainer"]{
-background:
-radial-gradient(black 15%, transparent 16%) 0 0,
-radial-gradient(black 15%, transparent 16%) 8px 8px,
-radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
-radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
-background-color:#282828;
-background-size:16px 16px;
+background: radial-gradient(circle at -8.9% 51.2%, rgb(255, 124, 0) 0%, rgb(255, 124, 0) 15.9%, rgb(255, 163, 77) 15.9%, rgb(255, 163, 77) 24.4%, rgb(19, 30, 37) 24.5%, rgb(19, 30, 37) 66%);
 </sytle>
 """
 
